98e7e237809409534b49313445623867f735e161
hapsitikle <hapsitikle@gmail.com>
2025-09-14 20:05:12 +1000

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -56,7 +56,7 @@ const Index = () => {
             style={{ animationDelay: "0.3s" }}
           >
             <img 
-              src="images/proof.jpg" 
+              src="images/fake noti.jpeg" 
               alt="Proof of rewards" 
               className="mx-auto w-48 md:w-64 h-auto rounded-lg shadow-lg"
             />
